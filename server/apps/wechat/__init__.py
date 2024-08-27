e6222aa244448d10aea476f4554ef5072372dce2
glenforev <glenforev@gmail.com>
2024-08-27 09:08:20 +0800

    delete all files

--- server/apps/wechat/__init__.py
+++
@@ -1,4 +0,0 @@
-# encoding: utf-8
-from __future__ import absolute_import, unicode_literals
-
-default_app_config = 'apps.wechat.apps.WechatConfig'
--- server/core/authentication.py
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-from rest_framework.authentication import SessionAuthentication
-
-
-class SessionAuthenticationWithOutCSRF(SessionAuthentication):
-
-    def enforce_csrf(self, request):
-        pass
